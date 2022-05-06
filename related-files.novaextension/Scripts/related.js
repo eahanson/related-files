aa10a3c694d3d86ac4f80732da8d001babcc6940
Erik Hanson <erik@eahanson.com>
2022-05-06 12:00:32 -0700

    Fix JS warning

--- related-files.novaextension/Scripts/related.js
+++ related-files.novaextension/Scripts/related.js
@@ -20,6 +20,7 @@ function findAnnotatedLines(text) {
 function findLinks(line) {
   const re = /[^\[]*\[([^\]]+)\]\(([^\)]+)\)/g;
   let result = [];
+  let match;
 
   while ((match = re.exec(line))) {
     result.push({ name: match[1], path: match[2] });
